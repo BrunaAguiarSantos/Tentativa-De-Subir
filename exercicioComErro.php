ee321520b56c9555e02878c7dd4b816d36c6c0a6
Bruna <124933198+BrunaAguiarSantos@users.noreply.github.com>
2023-03-15 19:05:57 -0300

    Add files via upload
    Não vejo a hora de aprender a subir pelo prompt T_T

---
+++ exercicioComErro.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Exemplo 2</title>
+</head>
+<body>
+    <?php
+    echo"Olá mundo que usa o PHP, ESSE É MEU PRIMEIRO PROGRAMA!"
+    echo"Espero aprender bastante sobre essa poderosa linguagem "
+    echo"durante as aulas"
+    ?>
+</body>
+/* erro do código --> O final dos comandos não tem ponto e vírgula*/
